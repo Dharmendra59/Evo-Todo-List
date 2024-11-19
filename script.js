e636df01d04fec7f1eb7af96b12ef8ead997ae6b
Dharmendra59 <singhdhas9559604443a@gmail.com>
2024-11-19 13:18:52 +0530

    ddd

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const wrapper = document.querySelector(".wrapper");
+const menuBtn = document.querySelector(".menu-btn");
+const backBtn = document.querySelector(".back-btn");
+const screen = document.querySelector(".screen");
+
+const toggleScreen = () => {
+      wrapper.classList.toggle("show-category");
+      
+};
+menuBtn.addEventListener("click", toggleScreen);
+backBtn.addEventListener("click", toggleScreen);
